be56dfbd635bf21175fec77a2d8f15d1cc73e0a9
Bhuwan <xyzzbit@gmail.com>
2023-02-15 10:36:49 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './index.css';
+import Button from './Button'
+// import Input from './Input';
+
+function App(){
+    return (
+        <div className='container'>
+            <div className='card'>
+            <Button/>  
+            </div>
+        </div>
+    );
+}
+export default App;
